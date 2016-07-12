d78fb9215ed186fadccffa84f9937633e5411a11
Izaac Garcilazo <izaacg@mail.sfsu.edu>
2016-07-12 22:19:37 +0000

    Bootstrap added

--- application/view/_templates/footer.php
+++ application/view/_templates/footer.php
@@ -7,7 +7,39 @@
         var url = "<?php echo URL; ?>";
     </script>
 
+    <!-- /.container -->
+
+    <div class="container">
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; BooksandTutors 2016</p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /.container -->
+    <!-- jQuery, loaded in the recommended protocol-less way -->
+    <!-- more http://www.paulirish.com/2010/the-protocol-relative-url/ -->
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+    <!-- define the project's URL (to make AJAX calls possible, even when using this in sub-folders etc) -->
+    <script>
+        var url = "<?php echo URL; ?>";
+    </script>
+
     <!-- our JavaScript -->
     <script src="<?php echo URL; ?>js/application.js"></script>
+
+    <!-- jQuery -->
+    <script src="<?php echo URL; ?>js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="<?php echo URL; ?>js/bootstrap.min.js"></script>
 </body>
 </html>
--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -1,30 +1,57 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
         <title>Books and Tutors SFSU</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 
-        <!-- JS -->
-        <!-- please note: The JavaScript files are loaded in the footer to speed up page construction -->
-        <!-- See more here: http://stackoverflow.com/q/2105327/1114320 -->
+    <!-- Custom CSS -->
+    <link href="css/shop-homepage.css" rel="stylesheet">
 
-        <!-- CSS -->
-        <link href="<?php echo URL; ?>css/style.css" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
     </head>
     <body>
-        <!-- Top Bar -->
-        <div>
-            <div class="logo">
-                <a href="<?php echo URL; ?>"><img src="<?php echo URL . '/img/logo.png' ?>" /></a>
+        <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#"><a href="<?php echo URL; ?>"><img src="<?php echo URL . '/img/logo.png' ?>" /></a></a>
             </div>
-            <!-- navigation -->
-            <div class="navigation">
-                <a href="<?php echo URL; ?>products">sell an item</a>
-                <a href="<?php echo URL; ?>cart">cart</a>
-                <a href="<?php echo URL; ?>customers">register</a>
-                <a href="<?php echo URL; ?>signin">sign in</a>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="<?php echo URL; ?>products">sell an item</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo URL; ?>cart">cart</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo URL; ?>customers">register</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo URL; ?>signin">sign in</a>
+                    </li>
+                </ul>
             </div>
             <div class="searchDiv">
                 <form action="<?php echo URL; ?>searchproducts/index" method="POST">
@@ -33,3 +60,7 @@
                 </form>
                 </div>
             </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
