2f80fdb7e33b06da261998c1fb14f14a7307e5b8
abrown <abrown@mail.sfsu.edu>
2016-07-20 09:37:04 +0000

    More progress to home and search page, and the header

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -1,68 +1,72 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
+    <head>
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
         <title>Books and Tutors SFSU</title>
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+        <!-- Bootstrap Core CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Custom CSS -->
-    <link href="css/shop-homepage.css" rel="stylesheet">
+        <!-- Custom CSS -->
+        <link href="css/shop-homepage.css" rel="stylesheet">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
     </head>
     <body>
         <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="student-warning">SFSU Software Engineering Project, Summer 2016.  For Demonstration Only</div>
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#"><a href="<?php echo URL; ?>"><img src="<?php echo URL . '/img/logo.png' ?>" /></a></a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li>
-                        <a href="<?php echo URL; ?>products">sell an item</a>
-                    </li>
-                    <li>
-                        <a href="<?php echo URL; ?>cart">cart</a>
-                    </li>
-                    <li>
-                        <a href="<?php echo URL; ?>customers">register</a>
-                    </li>
-                    <li>
-                        <a href="<?php echo URL; ?>signin">sign in</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="search-bar">
-                <form action="<?php echo URL; ?>searchproducts/index" method="POST">
-                    <input type="text" name='searchinput' placeholder="Search for books, tutors and more!" required>
-                    <input type="submit" name='submit_search_product' value="Search">
-                </form>
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <div class="student-warning">SFSU Software Engineering Project, Summer 2016.  For Demonstration Only</div>
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <div class="navbar-nav">
+                    <a class="navbar-brand header-logo" href="#"><a href="<?php echo URL; ?>"><img src="<?php echo URL . '/img/logo.png' ?>" /></a></a>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <div class="header-buttons">
+                    <ul class="nav navbar-nav">
+                        <li>
+                            <a href="<?php echo URL; ?>products">sell an item</a>
+                        </li>
+                        <li>
+                            <a href="<?php echo URL; ?>cart">cart</a>
+                        </li>
+                        <li>
+                            <a href="<?php echo URL; ?>customers">register</a>
+                        </li>
+                        <li>
+                            <a href="<?php echo URL; ?>signin">sign in</a>
+                        </li>
+                    </ul>
+                    </div>
+                    <br /><br /><br />
+                    <div class="navbar-nav header-search">
+                        <form action="<?php echo URL; ?>searchproducts/index" method="POST">
+                            <input type="text" name='searchinput' placeholder="Search for books, tutors and more!" required>
+                            <input type="submit" name='submit_search_product' value="Search">
+                        </form>
+                    </div>
                 </div>
             </div>
             <!-- /.navbar-collapse -->
         </div>
         <!-- /.container -->
     </nav>
-        
--- application/view/home/index.php
+++ application/view/home/index.php
@@ -1,81 +1,52 @@
 <!-- Page Content -->
-    <div class="container">
+<div class="container">
 
-        <div class="row">
+    <div class="row">
 
-            <div class="col-md-3">
-                <p class="lead">Shop Name</p>
-                <div class="list-group">
-                    <a href="#" class="list-group-item">Category 1</a>
-                    <a href="#" class="list-group-item">Category 2</a>
-                    <a href="#" class="list-group-item">Category 3</a>
-                </div>
+        <div class="col-md-3">
+            <p class="lead">Category</p>
+            <div class="list-group">
+                <a href="#" class="list-group-item">Books</a>
+                <a href="#" class="list-group-item">Tutors</a>
+                <a href="#" class="list-group-item">Electronics</a>
+                <a href="#" class="list-group-item">Entertainment</a>
+                <a href="#" class="list-group-item">Clothing</a>
+                <a href="#" class="list-group-item">Furniture</a>
+                <a href="#" class="list-group-item">Other</a>
             </div>
+        </div>
 
-            <div class="col-md-9">
-
-               <div class="row carousel-holder">
- 
-<!--                    <div class="col-md-12">
-                        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-                            <ol class="carousel-indicators">
-                                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-                            </ol>
-                            <div class="carousel-inner">
-                                <div class="item active">
-                                    <img class="slide-image" src="http://placehold.it/800x300" alt="">
+        <div class="col-md-9">
+            <div class="row">
+                <?php foreach ($products as $product) { ?>
+                    <!--<div class="col-sm-4 col-lg-4 col-md-4">-->
+                    <div class="thumbnail">
+                        <!--                            
+                        <?php
+                        if (isset($product->img1))
+                            echo '<img src="data:image/jpeg;base64,' . base64_encode($product->img1) . '"  height="320" width="150" />';
+                        else
+                            echo '<img src="<' . URL . '/img/logo.png" />';
+                        ?>
+                        -->
+                        <div class="caption">
+                            <h4>
+                                <div class="search-image">
+                                    <img src="<?php echo URL . '/img/imagenotfound.png' ?>" />
                                 </div>
-                                <div class="item">
-                                    <img class="slide-image" src="http://placehold.it/800x300" alt="">
-                                </div>
-                                <div class="item">
-                                    <img class="slide-image" src="http://placehold.it/800x300" alt="">
-                                </div>
-                            </div>
-                            <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-                                <span class="glyphicon glyphicon-chevron-left"></span>
-                            </a>
-                            <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-                                <span class="glyphicon glyphicon-chevron-right"></span>
-                            </a>
-                        </div>
-                    </div>-->
-
-                </div>
-
-                <div class="row">
-<?php foreach ($products as $product) { ?>
-                    <div class="col-sm-4 col-lg-4 col-md-4">
-                        <div class="thumbnail">
-                          <?php if (isset($product->img1)) echo '<img src="data:image/jpeg;base64,'.base64_encode($product->img1).'"  height="320" width="150" />';
-                          else
-                              echo '<img src="http://placehold.it/320x150">'; ?>
-                            <div class="caption">
-                                <h4 class="pull-right"><?php if (isset($product->price)) echo "$" . htmlspecialchars($product->price, ENT_QUOTES, 'UTF-8'); ?></h4>
-                                <h4><a href="#"><?php if (isset($product->name))
-                                    echo htmlspecialchars($product->name, ENT_QUOTES, 'UTF-8');
-                                ?></a>
-                                </h4>
-                                <p><?php if (isset($product->description)) echo htmlspecialchars($product->description, ENT_QUOTES, 'UTF-8'); ?></p>
-                            </div>
-<!--                            <div class="ratings">
-                                <p class="pull-right">15 reviews</p>
-                                <p>
-                                    <span class="glyphicon glyphicon-star"></span>
-                                    <span class="glyphicon glyphicon-star"></span>
-                                    <span class="glyphicon glyphicon-star"></span>
-                                    <span class="glyphicon glyphicon-star"></span>
-                                    <span class="glyphicon glyphicon-star"></span>
-                                </p>
-                            </div>-->
+                                <a href="#"><?php
+                                    if (isset($product->name))
+                                        echo htmlspecialchars($product->name, ENT_QUOTES, 'UTF-8');
+                                    ?></a></h4>
+                            <h4>
+                            <?php if (isset($product->price)) echo "$" . htmlspecialchars($product->price, ENT_QUOTES, 'UTF-8'); ?>
+                                 <input type="button" value="Add to cart" /></h4>
+                            <p><?php if (isset($product->description)) echo htmlspecialchars($product->description, ENT_QUOTES, 'UTF-8'); ?></p>
                         </div>
                     </div>
-            <?php } ?>
+                    <!--</div>-->
+                <?php } ?>
 
-
-                </div>
 
             </div>
 
@@ -83,40 +54,4 @@
 
     </div>
 
-
-<!--<div class="container">
-    <h2>LIST OF ITEMS</h3>
-         main content output 
-    <div class="box"> 
-        <table style="width:20%">
-            <tr>
-            <td><form action="<?php echo URL; ?>home/sortbypriceAsc" method="POST">
-                   <input type='submit' name='submit_sortbyprice_product' value="$ low/high" id="searchButton" />
-                </form></td>
-            <td><form action="<?php echo URL; ?>home/sortbypriceDesc" method="POST">
-                   <input type='submit' name='submit_sortbyprice_product' value="$ high/low" id="searchButton" />
-                </form></td>
-            </tr>
-        </table> 
-        <table>
-            <thead style="background-color: #ddd; font-weight: bold;"> 
-            <tr>
-                <td>Item</td>
-                <td>Description</td>
-                <td>Price</td>
-                <td>Stock Quantity</td>
-            </tr>
-            </thead>
-            <tbody>
-            <?php foreach ($products as $product) { ?>
-                <tr>
-		    <td><?php if (isset($product->name)) echo htmlspecialchars($product->name, ENT_QUOTES, 'UTF-8'); ?></td>
-                    <td><?php if (isset($product->description)) echo htmlspecialchars($product->description, ENT_QUOTES, 'UTF-8'); ?></td>
-                    <td><?php if (isset($product->price)) echo htmlspecialchars($product->price, ENT_QUOTES, 'UTF-8'); ?></td>
-                    <td><?php if (isset($product->stock_qty)) echo htmlspecialchars($product->stock_qty, ENT_QUOTES, 'UTF-8'); ?></td>
-                </tr>
-            <?php } ?>
-            </tbody>
-        </table>
-    </div>
-</div>-->
+</div>
