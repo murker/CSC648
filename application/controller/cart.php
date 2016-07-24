a7e1cbd4a1930b559360a1bb4f2b1cfa79a6c09a
jpuchin <jpuchin@sfsuswe.com>
2016-07-24 11:51:36 +0000

    Connected cart with user ID

--- application/controller/cart.php
+++ application/controller/cart.php
@@ -29,17 +29,31 @@ class Cart extends Controller {
     public function index() {
         
         // getting all songs and amount of songs
-        $cart_items = $this->cartmodel->getCartItems("666"); //$cid Hardcoded to 666
+
         //$amount_of_items = $this->cartmodel->getAmountOfItems();
         
         require APP . 'view/_templates/header.php';
+        $cart_items = $this->cartmodel->getCartItems($_SESSION['CurrentUser']); //$cid Hardcoded to 666
+        $products = array();
+        foreach ($cart_items as $item) {
+            array_push($products, $this->itemmodel->getProduct($item->product_id));
+        }
         require APP . 'view/cart/index.php';
         require APP . 'view/_templates/footer.php';
+        
+ 
+    }
+    
+    public function createCart() {
+        $this->cartmodel->createCart($_POST["uid"], $_POST["pid"],  $_POST["qty"]);
     }
 
     public function addItem() {
+        if (!isset($_SESSION)) {
+            session_start();
+        }
         if (isset($_POST["submit_add_item"])) {
-            $this->cartmodel->addCartItem($_POST["cid"], $_POST["pid"],  $_POST["qty"]);
+            $this->cartmodel->addCartItem($_SESSION['CurrentUser'], $_POST["pid"],  $_POST["qty"]);
         }
 
         // where to go after song has been added
@@ -48,10 +62,13 @@ class Cart extends Controller {
     }
 
     public function removeItem() {
+        if (!isset($_SESSION)) {
+            session_start();
+        }
         // if we have an id of a song that should be deleted
         if (isset($_POST["submit_delete_item"])) {
             // do deleteSong() in model/cartModel.php
-            $this->cartmodel->deleteCartItem($_POST["cid"], $_POST["pid"]);
+            $this->cartmodel->deleteCartItem($_SESSION['CurrentUser'], $_POST["pid"]);
         }
 
         // where to go after song has been deleted
