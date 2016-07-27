a82071579955c22adf42d185f54c954b73e962a6
abrown <abrown@mail.sfsu.edu>
2016-07-27 03:15:52 +0000

    New category skeleton

--- application/view/home/index.php
+++ application/view/home/index.php
@@ -14,7 +14,7 @@
         </div>
     <br /><br />
         <div class="row">
-            <div class="col-sm-2">
+<!--            <div class="col-sm-2">
                 <div class="list-group">   
                     <form action="<?php echo URL; ?>home/sortbyCategory" method="GET">
                         <input type='submit' class="list-group-item" name='submit_sortbyBooks' value="Books" />
@@ -38,9 +38,9 @@
                         <input type='submit' class="list-group-item" name='submit_sortbyOther' value="Other" />
                     </form>
                 </div>
-            </div>
-            <div class="col-sm-1"></div>
-            <div class="col-sm-9">
+            </div>-->
+            <div class="col-sm-2"></div>
+            <div class="col-sm-8">
 
                 <div class="row">
                     <?php foreach ($products as $product) { ?>
@@ -63,6 +63,7 @@
                                         <h5>
                                             <?php if (isset($product->price)) echo "$" . htmlspecialchars($product->price, ENT_QUOTES, 'UTF-8'); ?>
                                         </h5>
+                                        <span class = "quantity"><?php if (isset($product->stock_qty)) echo htmlspecialchars($product->stock_qty, ENT_QUOTES, 'UTF-8'); ?> available</span>
                                         <!--<br /><br /><input type="button" value="Add to cart" class="btn btn-primary"/>-->
                                     <!--<p><?php if (isset($product->description)) echo htmlspecialchars($product->description, ENT_QUOTES, 'UTF-8'); ?></p>-->
                                     </div>
--- application/view/item/index.php
+++ application/view/item/index.php
@@ -20,23 +20,25 @@
             ?>  
         </div>
         <div class ="col-sm-7">
-            <h4><?php
+            <h3><?php
                 if (isset($product->name))
                     echo htmlspecialchars($product->name, ENT_QUOTES, 'UTF-8');
                 ?>
+            </h3>
+            <h4>
+                <?php if (isset($product->price)) echo "$" . htmlspecialchars($product->price, ENT_QUOTES, 'UTF-8'); ?>
             </h4>
-            <p><?php if (isset($product->description)) echo htmlspecialchars($product->description, ENT_QUOTES, 'UTF-8'); ?></p>
             <form action="<?php echo URL; ?>cart/additem" method="POST">
+                <span class = "quantity"><?php if (isset($product->stock_qty)) echo htmlspecialchars($product->stock_qty, ENT_QUOTES, 'UTF-8'); ?> available</span>
+                <br /> <br />
                 <label for='qty' >Quantity: </label>
-                <input type='text' name='qty' id='quantity' value="1" required />
-                <label for='stock_qty' >available: </label>
-                <?php if (isset($product->stock_qty)) echo htmlspecialchars($product->stock_qty, ENT_QUOTES, 'UTF-8'); ?>
-                <h5>
-                    <?php if (isset($product->price)) echo "$" . htmlspecialchars($product->price, ENT_QUOTES, 'UTF-8'); ?>
-                </h5>
+                <input type='text' name='qty' value="1" class="quantity-text" required />
+                <br />
                 <input type="hidden" name="pid" value="<?php echo htmlspecialchars($product->id, ENT_QUOTES, 'UTF-8'); ?>" />
-                <input type="button" value="Buy It Now" class = "btn btn-primary"/></br>
-                <input type="submit" name="submit_add_item" value="Add to cart" class = "btn btn-primary" />      
+                <input type="submit" name="submit_add_item" value="Add to cart" class = "btn btn-warning" />     
+                <input type="button" value="Buy It Now" class = "btn btn-primary"/>
+                <h6 style="visibility: hidden">-</h6>
+                <p><?php if (isset($product->description)) echo htmlspecialchars($product->description, ENT_QUOTES, 'UTF-8'); ?></p>
             </form>        
         </div>
     </div>
