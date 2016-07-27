f65d501a388317c744ab1efa5e0faf762d42168d
abrown <abrown@mail.sfsu.edu>
2016-07-27 09:14:24 +0000

    Try to restore some changes that were lost due to Netbean's fkn caching

--- application/view/invoice/index.php
+++ application/view/invoice/index.php
@@ -1,7 +1,34 @@
+<td><form action="<?php echo URL; ?>invoice/getInvoice" method="GET">
+        <input type='submit' name='getInvoice' value="show invoice" id="searchButton" />
+    </form></td>
+
+<p>Set to my personal sfsu email for testing. It works. To test go to controller and edit email to and from. This buttons will go away once sfsu email checking is implemented.<p>                
+<td><form action="<?php echo URL; ?>invoice/sendsellerConfirmation" method="GET">
+        <input type='submit' name='sendsellerConfirmation' value="send seller confirmation Email" id="searchButton" />
+    </form></td>
+<td><form action="<?php echo URL; ?>invoice/sendbuyerConfirmation" method="GET">
+        <input type='submit' name='sendbuyerConfirmation' value="send buyer confirmation Email" id="searchButton" />
+    </form></td>
+
+
 <div class="container">
-    <h2>Invoice</h2>
-    <!-- add sign in form -->                  
-    <div class="box"> 
-        <p><strong>Thank you for your purchase.</strong></p>
+    <div>
+        <h3>Invoice</h3>       
+        <label>Customer_id: </label>
+        <?php if (isset($invoice->customer_id)) echo htmlspecialchars($invoice->customer_id, ENT_QUOTES, 'UTF-8'); ?><br>    
+        <label>Cart_id: </label>
+        <?php if (isset($invoice->cart_id)) echo htmlspecialchars($invoice->cart_id, ENT_QUOTES, 'UTF-8'); ?><br>
+        <label>Total: </label>
+        <?php if (isset($invoice->total)) echo htmlspecialchars($invoice->total, ENT_QUOTES, 'UTF-8'); ?><br>
+        <label>Shipping cost: </label>
+        <?php if (isset($invoice->order_date)) echo htmlspecialchars($invoice->order_date, ENT_QUOTES, 'UTF-8'); ?><br>
+        <label>Shipping cost: </label>
+        <?php if (isset($invoice->shipping_cost)) echo htmlspecialchars($invoice->shipping_cost, ENT_QUOTES, 'UTF-8'); ?><br>
+        <label>Tax: </label>
+        <?php if (isset($invoice->tax)) echo htmlspecialchars($invoice->tax, ENT_QUOTES, 'UTF-8'); ?><br>
+        <label>Total: </label>
+        <?php if (isset($invoice->grand_total)) echo htmlspecialchars($invoice->grand_total, ENT_QUOTES, 'UTF-8'); ?><br>
     </div>
+    This invoice has been sent to your email <?php if (isset($customer->email)) echo htmlspecialchars($customer->email, ENT_QUOTES, 'UTF-8'); ?><br>
+    Thank you for shopping with us.
 </div>
--- application/view/payment/index.php
+++ application/view/payment/index.php
@@ -1,7 +1,71 @@
+<?php
+$subtotal = 0;
+$tax = 0.09;
+?>
 <div class="container">
     <div class="row">
         <div class="col-sm-2"></div>
         <div class="col-sm-8">
+            <div clss="box">
+                <h3>Your items for sale</h3>
+                <table>
+                    <thead style="background-color: #ddd; font-weight: bold;">
+                        <tr>
+                            <td>Item</td>                
+                            <td></td> 
+                            <td></td>
+                            <td>Price</td>
+                            <td>Qty</td>
+                            <td>Total</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($products as $product) { ?>
+                            <tr>
+                                <td><?php if (isset($product->img1) && $product->img1 != "") echo '<img src="data:image/jpeg;base64,' . base64_encode($product->img1) . '"  height="42" width="42" />'; ?></td>                    
+                                <td><?php if (isset($product->name)) echo htmlspecialchars($product->name, ENT_QUOTES, 'UTF-8'); ?></td>                   
+                                <td><?php if (isset($product->price)) {
+                            $price = htmlspecialchars($product->price, ENT_QUOTES, 'UTF-8');
+                            echo $price;
+                        } ?></td>                                       
+                                <td><?php if (isset($product->qty)) {
+                                $quantity = htmlspecialchars($product->qty, ENT_QUOTES, 'UTF-8');
+                                echo $quantity;
+                            } ?></td>   
+                                <td><?php $total = $price * $quantity;
+                            echo number_format((float) $total, 2, '.', ''); ?></td>
+    <?php $subtotal += $total; ?>
+                            </tr>
+<?php } ?> 
+
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td style="font-weight:bold">Subtotal:</td>
+                            <td></td>
+                            <td><?php echo number_format((float) $subtotal, 2, '.', '') ?></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td style="font-weight:bold">Tax:</td>
+                            <td></td>
+                            <td><?php echo number_format((float) $subtotal * $tax, 2, '.', '') ?></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td style="font-weight:bold">Total:</td>
+                            <td></td>
+                            <td><?php echo number_format((float) $subtotal + ($subtotal * $tax), 2, '.', '') ?></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <br />
             <div class="row">
                 <form action="<?php echo URL; ?>payment/finishtransaction" method="POST">
                     <h4 class="title">Payment</h4>
@@ -51,7 +115,7 @@
                             <label>Street Address</label>
                         </div>
                         <div class="col-sm-9">
-                            <input type='text' name='street' id='street' value="" required />
+                            <input type='text' name='street' id='street' value="<?php echo htmlspecialchars($customer->street, ENT_QUOTES, 'UTF-8'); ?>" required />
                         </div>
                     </div>
                     <div>
@@ -59,14 +123,14 @@
                             <label>City</label>
                         </div>
                         <div class="col-sm-9">
-                            <input type='text' name='city' id='city' value="" required />
+                            <input type='text' name='city' id='city' value="<?php echo htmlspecialchars($customer->city, ENT_QUOTES, 'UTF-8'); ?>" required />
                         </div>
                         <div>
                             <div class="col-sm-3">
                                 <label>Zip Code</label>
                             </div>
                             <div class="col-sm-9">
-                                <input type='text' name='zipcode' id='zipcode' value="" required />
+                                <input type='text' name='zipcode' id='zipcode' value="<?php echo htmlspecialchars($customer->zipcode, ENT_QUOTES, 'UTF-8'); ?>" required />
                             </div>
                         </div>
                         <div class="clear"> </div>
