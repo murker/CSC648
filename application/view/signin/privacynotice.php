0068f3872cbcd0c699a6c9142f37137d553ed2ce
Marc Anthony Garde <mgarde@mail.sfsu.edu>
2016-07-28 00:44:07 -0700

    added privacy notice

--- application/view/signin/privacynotice.php
+++ application/view/signin/privacynotice.php
@@ -1,7 +1,100 @@
 <div class="container">
-    <h2>PRIVACY NOTICE</h2>
-    <!-- add sign in form -->                  
-    <div class="box"> 
-        <p><strong>Privacy notice coming soon</strong></p>
+    <h2>Books and Tutors SFSU Privacy Notice</h2>                 
+    <div class="box">
+        <style>
+            #body
+            {
+                font-size:11pt;
+                width:100%;
+                margin:0 auto;
+                text-align:justify;
+            }
+
+            #header
+            {
+                font-family:verdana;
+                font-size:21pt;
+                width:100%;
+                margin:0 auto;
+            }
+
+            .consistent
+            {
+                display:none;
+            }
+        </style>
+        <div id='body'>
+            <div style='clear:both;height:10px;'></div>
+            <div class='innerText'>This privacy policy has been compiled to better serve 
+                those who are concerned with how their 'Personally Identifiable Information' 
+                (PII) is being used online. PII, as described in US privacy law and information 
+                security, is information that can be used on its own or with other information 
+                to identify, contact, or locate a single person, or to identify an individual 
+                in context. Please read our privacy policy carefully to get a clear understanding 
+                of how we collect, use, protect or otherwise handle your Personally Identifiable 
+                Information in accordance with our website.<br>
+            </div>
+
+            <span id='infoCo'></span><br>
+            <div class='grayText'><strong>What personal information do we collect from the 
+                    people that visit our blog, website or app?</strong>
+            </div>
+            <div class='innerText'>When ordering or registering on our site, as appropriate, 
+                you may be asked to enter your name, email address, mailing address, phone number, 
+                credit card information  or other details to help you with your experience.
+            </div><br>
+
+            <div class='grayText'><strong>When do we collect information?</strong>
+            </div>
+            <div class='innerText'>We collect information from you when you register on our 
+                site, place an order or enter information on our site.
+            </div><br> <span id='infoUs'></span>
+
+            <div class='grayText'><strong>How do we use your information? </strong>
+            </div>
+            <div class='innerText'> We may use the information we collect from you 
+                when you register and make a purchase in the following ways:<br>
+            </div>
+
+            <div class='innerText'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <strong>&bull;</strong> To 
+                personalize your experience and to allow us to deliver the type of content and 
+                product offerings in which you are most interested.
+            </div>
+            <div class='innerText'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <strong>&bull;</strong> To 
+                improve our website in order to better serve you.
+            </div>
+            <div class='innerText'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <strong>&bull;</strong> 
+                To allow us to better service you in responding to your customer service requests.
+            </div>
+            <div class='innerText'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <strong>&bull;</strong> 
+                To quickly process your transactions.
+            </div>
+            <div class='innerText'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <strong>&bull;</strong> To ask 
+                for ratings and reviews of services or products</div><span id='trDi'></span><br>
+
+            <div class='grayText'><strong>Third-party disclosure</strong>
+            </div>
+            <div class='innerText'>We do not sell, trade, or otherwise transfer to outside parties 
+                your Personally Identifiable Information.
+            </div><span id='trLi'></span><br>
+
+            <div class='grayText'><strong>Third-party links</strong>
+            </div>
+            <div class='innerText'>We do not include or offer third-party products or services on 
+                our website.
+            </div><span id='gooAd'></span><br> 
+
+            <div class='blueText'><strong>Contacting Us</strong>
+            </div>
+            <div class='innerText'>If there are any questions regarding this privacy policy, you may contact 
+                us using the information below.<br><br>
+            </div>
+
+            <div class='innerText'>http://sfsuswe.com/~su16g04/m3/</div>
+            <div class='innerText'>1600 Holloway Ave</div>San Francisco, CA 94132 
+            <div class='innerText'>
+            </div>
+            <div class='innerText'><br>Last Edited on 2016-07-27</div>
+        </div>
     </div>
 </div>
