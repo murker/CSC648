8acc31621f49a1d217b2fbd010b2b8fbb1b7833e
abrown <abrown@mail.sfsu.edu>
2016-07-28 04:08:39 +0000

    Item view cleanup for multiple images and tutor form

--- application/view/item/index.php
+++ application/view/item/index.php
@@ -4,21 +4,21 @@
         <div class="col-sm-4">
             <?php
             if (isset($product->img1) && $product->img1 != "")
-                echo '<img src="data:image/jpeg;base64,' . base64_encode($product->img1) . '" class = "img-thumbnail img-responsive"/>';
+                echo '<img src="data:image/jpeg;base64,' . base64_encode($product->img1) . '" class = "img-thumbnail img-responsive pull-left"/>';
             ?>
             <?php if ($product->category_id != 2) : ?>
-            <?php
-            if (isset($product->img2) && $product->img2 != "")
-                echo '<img src="data:image/jpeg;base64,' . base64_encode($product->img2) . '" class = "img-thumbnail img-responsive"/>';
-            ?>
-            <?php
-            if (isset($product->img3) && $product->img3 != "")
-                echo '<img src="data:image/jpeg;base64,' . base64_encode($product->img3) . '" class = "img-thumbnail img-responsive"/>';
-            ?>
-            <?php
-            if (isset($product->img4) && $product->img4 != "")
-                echo '<img src="data:image/jpeg;base64,' . base64_encode($product->img4) . '" class = "img-thumbnail img-responsive"/>';
-            ?>
+                <?php
+                if (isset($product->img2) && $product->img2 != "")
+                    echo '<img src="data:image/jpeg;base64,' . base64_encode($product->img2) . '" class = "img-thumbnail img-responsive sub-image" style="clear:left" />';
+                ?>
+                <?php
+                if (isset($product->img3) && $product->img3 != "")
+                    echo '<img src="data:image/jpeg;base64,' . base64_encode($product->img3) . '" class = "img-thumbnail img-responsive sub-image"/>';
+                ?>
+                <?php
+                if (isset($product->img4) && $product->img4 != "")
+                    echo '<img src="data:image/jpeg;base64,' . base64_encode($product->img4) . '" class = "img-thumbnail img-responsive sub-image"/>';
+                ?>
             <?php endif; ?>
         </div>
         <div class ="col-sm-6">
@@ -45,62 +45,47 @@
                         <input type="submit" name="submit_add_item" value="Add to cart" class = "btn btn-warning" />     
                         <input type="button" value="Buy It Now" class = "btn btn-primary"/>
                     <?php endif; ?> 
-                    <h6 style="visibility: hidden">-</h6>
+                    <br />
                     <p><?php if (isset($product->description)) echo htmlspecialchars($product->description, ENT_QUOTES, 'UTF-8'); ?></p>
                 </form> 
                 <?php if ($product->category_id == 2) : ?>
-                    <h3>Contact</h3>
-                    <p>To contact me please use the contact form below.</p>
-                    <form name="contactform" method="POST" action="">
-                        <table width="450px">
-                            <tr>
-                                <td valign="top">
-                                    <label for="first_name">First Name *</label>
-                                </td>
-                                <td valign="top">
-                                    <input  type="text" name="first_name" maxlength="50" size="30" value = "<?php if (isset($customer->firstname)) echo htmlspecialchars($customer->firstname, ENT_QUOTES, 'UTF-8'); ?>">
-                                </td>
-                            </tr>
-                            <tr>
-                                <td valign="top"">
-                                    <label for="last_name">Last Name *</label>
-                                </td>
-                                <td valign="top">
-                                    <input  type="text" name="last_name" maxlength="50" size="30" value = "<?php if (isset($customer->lastname)) echo htmlspecialchars($customer->lastname, ENT_QUOTES, 'UTF-8'); ?>">
-                                </td>
-                            </tr>
-                            <tr>
-                                <td valign="top">
-                                    <label for="email">Email Address *</label>
-                                </td>
-                                <td valign="top">
-                                    <input  type="text" name="email" maxlength="80" size="30" value = "<?php if (isset($customer->email)) echo htmlspecialchars($customer->email, ENT_QUOTES, 'UTF-8'); ?>">
-                                </td>
-                            </tr>
-                            <tr>
-                                <td valign="top">
-                                    <label for="telephone">Telephone Number *</label>
-                                </td>
-                                <td valign="top">
-                                    <input  type="text" name="telephone" maxlength="30" size="30" value = "<?php if (isset($customer->phone)) echo htmlspecialchars($customer->phone, ENT_QUOTES, 'UTF-8'); ?>">
-                                </td>
-                            </tr>
-                            <tr>
-                                <td valign="top">
-                                    <label for="comments">Comments *</label>
-                                </td>
-                                <td valign="top">
-                                    <textarea  name="comments" maxlength="1000" cols="25" rows="6"></textarea>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td colspan="2" style="text-align:center">
-                                    <input type="submit" value="Submit">   
-                                </td>
-                            </tr>
-                        </table>
+                    <br />
+                    <h4>Contact</h4>
+                    <p>Please use the form below to contact this tutor</p>
+                    <form class="form-horizontal" name="contactform" method="POST" action="">
+                        <div class="form-group">
+                            <label  class="col-sm-2 control-label">First Name</label>
+                            <div class="col-sm-8">
+                                <input  class="form-control" type="text" name="first_name" maxlength="50" size="30" value = "<?php if (isset($customer->firstname)) echo htmlspecialchars($customer->firstname, ENT_QUOTES, 'UTF-8'); ?>">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label  class="col-sm-2 control-label">Last Name</label>
+                            <div class="col-sm-8">
+                                <input  class="form-control" type="text" name="last_name" maxlength="50" size="30" value = "<?php if (isset($customer->lastname)) echo htmlspecialchars($customer->lastname, ENT_QUOTES, 'UTF-8'); ?>">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label  class="col-sm-2 control-label">Email</label>
+                            <div class="col-sm-8">
+                                <input  class="form-control" type="text" name="email" maxlength="80" size="30" value = "<?php if (isset($customer->email)) echo htmlspecialchars($customer->email, ENT_QUOTES, 'UTF-8'); ?>">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label  class="col-sm-2 control-label">Phone</label>
+                            <div class="col-sm-8">
+                                <input  class="form-control" type="text" name="telephone" maxlength="30" size="30" value = "<?php if (isset($customer->phone)) echo htmlspecialchars($customer->phone, ENT_QUOTES, 'UTF-8'); ?>">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label  class="col-sm-2 control-label">Comments</label>
+                            <div class="col-sm-8">
+                                <textarea  class="form-control" name="comments" maxlength="1000" cols="25" rows="6"></textarea>
+                            </div>
+                        </div>
+                        <input type="submit" value="Submit" class="btn btn-primary">   
                     </form>           
                 <?php endif; ?>
-     </div>
+        </div>
     </div>
 </div>
