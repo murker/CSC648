18ae9944d3745e4ca12e42cfb1d0870d863f5c04
Izaac Garcilazo <izaacg@mail.sfsu.edu>
2016-08-01 17:00:15 +0000

    repeted sql calls erased

--- application/dbcalls/sqlcalls.php
+++ application/dbcalls/sqlcalls.php
@@ -197,18 +197,6 @@ class Sqlcalls {
         $query->execute($parameters);
     }
 
-    public function getAllProducts() {
-        $sql = "SELECT id, name, description, price, stock_qty, category_id, img1 FROM product";
-        $query = $this->db->prepare($sql);
-        $query->execute();
-
-        // fetchAll() is the PDO method that gets all result rows, here in object-style because we defined this in
-        // core/controller.php! If you prefer to get an associative array as the result, then do
-        // $query->fetchAll(PDO::FETCH_ASSOC); or change core/controller.php's PDO options to
-        // $options = array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC ...
-        return $query->fetchAll();
-    }
-
     public function sortbyCategory($parameters) {
         $sql = "SELECT id, name, description, price, stock_qty, category_id, img1 FROM product where category_id = :category";
         $query = $this->db->prepare($sql);
@@ -359,26 +347,16 @@ class Sqlcalls {
         $query->execute($parameters);
     }
 
-//    public function getAllProducts() {
-//        $sql = "SELECT id, name, description, price, stock_qty, img1, img2, img3, img4, category_id FROM product";
-//        $query = $this->db->prepare($sql);
-//        $query->execute();
-//        // fetchAll() is the PDO method that gets all result rows, here in object-style because we defined this in
-//        // core/controller.php! If you prefer to get an associative array as the result, then do
-//        // $query->fetchAll(PDO::FETCH_ASSOC); or change core/controller.php's PDO options to
-//        // $options = array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC ...
-//        return $query->fetchAll();
-//    }
-
-//    public function getProduct($parameters) {
-//        $sql = "SELECT id, name, description, price, stock_qty, img1, img2, img3, img4, category_id FROM product WHERE id = :product_id LIMIT 1";
-//        $query = $this->db->prepare($sql);
-//        // useful for debugging: you can see the SQL behind above construction by using:
-//        // echo '[ PDO DEBUG ]: ' . Helper::debugPDO($sql, $parameters);  exit();
-//        $query->execute($parameters);
-//        // fetch() is the PDO method that get exactly one result
-//        return $query->fetch();
-//    }
+    public function getAllProducts() {
+        $sql = "SELECT id, name, description, price, stock_qty, img1, img2, img3, img4, category_id FROM product";
+        $query = $this->db->prepare($sql);
+        $query->execute();
+        // fetchAll() is the PDO method that gets all result rows, here in object-style because we defined this in
+        // core/controller.php! If you prefer to get an associative array as the result, then do
+        // $query->fetchAll(PDO::FETCH_ASSOC); or change core/controller.php's PDO options to
+        // $options = array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC ...
+        return $query->fetchAll();
+    }
     
     public function searchProductW($parameters) {
         $sql = "SELECT id, name, description, price, stock_qty, category_id, img1 FROM product WHERE name like :searchword";
