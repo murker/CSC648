6f24788bfc6ea763a6476fb7ba9d94d09fc2e462
Izaac Garcilazo <izaacg@mail.sfsu.edu>
2016-08-04 00:31:39 +0000

    buy it now button fixed

--- application/view/home/index.php
+++ application/view/home/index.php
@@ -86,6 +86,7 @@
                                                 <?php endif; ?>
                                                 <br />
                                                 <input type="hidden" name="pid" value="<?php echo htmlspecialchars($product->id, ENT_QUOTES, 'UTF-8'); ?>" />
+                                                <input type="hidden" name="qty" value= 1 />
                                                 <input type="submit" name="submit_buyitnow" value="Buy It Now" class = "btn btn-warning"/>
                                             </form>
                                         <?php endif; ?>
